9e6a75d762a06e2194b34e6ace7be4a28fae8d99
rapsealk <piono623@naver.com>
2021-06-11 04:09:31 +0900

    Heartbeat 메시지 추가

--- protos/compile.sh
+++ protos/compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+./protoc -I ./ --csharp_out=. $1 --grpc_out=. --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. $1 --plugin=protoc-gen-grpc=grpc_python_plugin
