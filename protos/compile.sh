e899ea57e05b8e9da98e4b77a1bbb747fe1fca3a
rapsealk <piono623@naver.com>
2021-06-16 22:49:37 +0900

    WIP: 장애물 위치 정보 추가

--- protos/compile.sh
+++ protos/compile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-./protoc -I ./ --csharp_out=. $1 --grpc_out=. --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. $1 --plugin=protoc-gen-grpc=grpc_python_plugin
+./protoc -I ./ --csharp_out=. $1 --grpc_out=. --plugin=protoc-gen-grpc=./grpc_csharp_plugin
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. $1 --plugin=protoc-gen-grpc=./grpc_python_plugin
+
+mv ./*.py ../../gym-navops/gym_navops/envs
+mv ./*.cs ../../NavOps/Assets/Scripts/Grpc
